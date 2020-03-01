7239bb7dd0eac699ee7b633521643b2c32bcba55
Beza Sirak <sirak.bezawit@gmail.com>
2020-03-01 09:35:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -29,17 +29,7 @@ def map_to_square(source_map)
   source_map
 end
 
-# def reduce_to_total(source_array)
-#   index = 0
-#   total = 0
-#   while index < source_array.length do
-#     total += source_array[index]
-    
-#     index += 1 
-#   end
-#   total
-# end
-def reduce_to_total(source_array, starting_point=0)
+def reduce_to_total(source_array, starting_point = 0)
   index = 0
   total = starting_point
   while index < source_array.length do
