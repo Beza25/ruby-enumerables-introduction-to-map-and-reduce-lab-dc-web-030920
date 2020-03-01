bab87b3e28fbcddeb9550fb838b6a31f38fe8141
Beza Sirak <sirak.bezawit@gmail.com>
2020-03-01 09:23:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,68 @@
 # My Code here....
+
+def map_to_negativize(source_map)
+  
+  index = 0 
+  while index < source_map.length do 
+    source_map[index] = source_map[index] * -1
+    index += 1
+  end 
+  source_map
+end
+def map_to_no_change(source)
+  source
+end
+def map_to_double(source_map)
+  index = 0 
+  while index < source_map.length do
+    source_map[index] = source_map[index] * 2
+    index += 1 
+  end
+  source_map
+end
+def map_to_square(source_map)
+  index = 0 
+  while index < source_map.length do
+    source_map[index] = source_map[index] * source_map[index]
+    index +=  1 
+  end
+  source_map
+end
+
+def reduce_to_total(source_array)
+  index = 0
+  total = 0
+  while index < source_array.length do
+    total += source_array[index]
+    
+    index += 1 
+  end
+  total
+end
+def reduce_to_total(source_array, starting_point)
+  index = 0
+  total = starting_point
+  while index < source_array.length do
+    total += source_array[index]
+    
+    index += 1 
+  end
+  total
+end
+def reduce_to_all_true(source_array)
+  index = 0 
+  while index < source_array.length do
+    if source_array[index] == false 
+      return false
+    end
+    index += 1 
+  end
+  true
+end
+def reduce_to_any_true(source_array)
+  source_array.any?{ |value| 
+    value == true
+    return true
+  }
+  false
+end
